db486b165a4a249d8b9e034e468d992076c98dca
Galih Anggoro Prasetya <83481679+galihap76@users.noreply.github.com>
2023-12-04 06:38:00 +0700

    Update Main.java

--- 22205018-PBO-Lat54/src/Main.java
+++ 22205018-PBO-Lat54/src/Main.java
@@ -7,61 +7,81 @@
  * Deskripsi Program : Program ini membuat koordinat sederhana pada java.
 */
 
+// Mendefinisikan kelas Koordinat
 class Koordinat {
+	
+	// Set property x, y ke protected
     protected int x;
     protected int y;
     
+    // Konstruktor untuk inisialisasi objek Koordinat dengan nilai x dan y
     public Koordinat(int x, int y){
         
     }
 
+    // Getter untuk nilai x
     public int getX() {
         return x;
     }
 
+    // Setter untuk nilai x
     public void setX(int x) {
         this.x = x;
     }
 
+    // Getter untuk nilai y
     public int getY() {
         return y;
     }
 
+    // Setter untuk nilai y
     public void setY(int y) {
         this.y = y;
     }
 }
 
+// Mendefinisikan kelas WarnaKoordinat yang merupakan turunan dari Koordinat
 class WarnaKoordinat extends Koordinat {
+	
+    // Set property string ke public
     public String namaWarna;
 
+    // Konstruktor untuk inisialisasi objek WarnaKoordinat dengan nilai x, y, dan namaWarna
     public WarnaKoordinat(int x, int y, String namaWarna){
-     super (x, y);
+        // Memanggil konstruktor kelas induk (Koordinat)
+        super(x, y);
     }
+
+    // Getter untuk namaWarna
     public String getNamaWarna() {
         return namaWarna;
     }
 
+    // Setter untuk namaWarna
     public void setNamaWarna(String namaWarna) {
         this.namaWarna = namaWarna;
     }
-    
-    
 }
 
-
+// Buat kelas Main
 public class Main {
 
-	public static void main(String[] args) {
-		
-	    WarnaKoordinat wk = new  WarnaKoordinat(0,0,"");
+	// Jalankan method utama
+    public static void main(String[] args) {
+        
+        // Instansiasi objek WarnaKoordinat
+        WarnaKoordinat wk = new WarnaKoordinat(0,0,"");
+        
+        // Mengatur namaWarna
         wk.setNamaWarna("Jingga");
+        
+        // Mengatur nilai x dan y
         wk.setX(10);
         wk.setY(4);
+        
+        // Menampilkan informasi Warna Koordinat
         System.out.println("Warna Koordinat : " + wk.getNamaWarna());
-        System.out.println("Koordinat Sumbu x : " + wk.getX() + ", y : " 
-                            + wk.getY());
+        System.out.println("Koordinat Sumbu x : " + wk.getX() + ", y : " + wk.getY());
 
-	}
-
+    }
 }
