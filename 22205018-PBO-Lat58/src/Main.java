d47eb6c7094d8f2e1670be151613a1a2b2150bb6
Galih Anggoro Prasetya <83481679+galihap76@users.noreply.github.com>
2023-12-04 09:59:19 +0700

    Update Main.java

--- 22205018-PBO-Lat58/src/Main.java
+++ 22205018-PBO-Lat58/src/Main.java
@@ -7,48 +7,66 @@
  * Deskripsi Program : Program tentang tambah kurang pada java.
 */
 
+// Membuat kelas Bilangan
 class Bilangan {
+	
+	// Buat property x, y 
    private int x;
    private int y;
    
+   // Konstruktor untuk menginisialisasi nilai x dan y
    public Bilangan(){
+	   
+	   // Tampung nilai menggunakan this yang merujuk pada property x menjadi 3, dan y menjadi 4
        this.x = 3;
        this.y = 4;
    }
 
+    // Getter untuk nilai x
     public int getX() {
         return x;
     }
 
+    // Getter untuk nilai y
     public int getY() {
         return y;
     }
 }
 
+// Membuat kelas JumlahBilangan yang merupakan turunan dari Bilangan
 class JumlahBilangan extends Bilangan{
+	
+    // Metode untuk menampilkan hasil penjumlahan x dan y
     public void tampilHasilJumlah(){
-        System.out.println("Hasil Perjumlahan = "+ (getX()+getY()));
+        System.out.println("Hasil Perjumlahan = " + (getX() + getY()));
     }
-    
 }
 
+// Membuat kelas SelisihBilangan yang merupakan turunan dari Bilangan
 class SelisihBilangan extends Bilangan {
+	
+    // Metode untuk menampilkan hasil selisih x dan y
     public void tampilSelisih(){
-        System.out.println("Hasil Selisih " +getX()+" - "+getY()+" = "
-                            +(getX()-getY()));
-   }
+        System.out.println("Hasil Selisih " + getX() + " - " + getY() + " = " + (getX() - getY()));
+    }
 }
 
+// Kelas Main sebagai kelas utama
 public class Main {
 
-	public static void main(String[] args) {
-		
-		   JumlahBilangan bil = new JumlahBilangan();
-	       bil.tampilHasilJumlah();
-	       
-	       SelisihBilangan blg = new SelisihBilangan();
-	       blg.tampilSelisih();
-
-	}
-
+	// Method main sebagai method utama untuk menjalankan program
+    public static void main(String[] args) {
+        
+        // Membuat objek JumlahBilangan
+        JumlahBilangan bil = new JumlahBilangan();
+        
+        // Menampilkan hasil penjumlahan
+        bil.tampilHasilJumlah();
+        
+        // Membuat objek SelisihBilangan
+        SelisihBilangan blg = new SelisihBilangan();
+        
+        // Menampilkan hasil selisih
+        blg.tampilSelisih();
+    }
 }
