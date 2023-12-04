37fc90fc3075b2ce3d80ad8eef48cadf461ec0b8
Galih Anggoro Prasetya <83481679+galihap76@users.noreply.github.com>
2023-12-04 10:45:34 +0700

    Update Main.java

--- 22205018-PBO-Lat60/src/Main.java
+++ 22205018-PBO-Lat60/src/Main.java
@@ -7,69 +7,90 @@
  * Deskripsi Program : Program ini berisi tentang anggota akatsuki pada java.
 */
 
+// Import library java array list
 import java.util.ArrayList;
 import java.util.List;
 
+// Mendefinisikan kelas Akatsuki
 class Akatsuki {
+	
+	// Membuat property pakaian
+ private String pakaian;
 
-    private String pakaian;
+ // Konstruktor untuk menginisialisasi informasi pakaian
+ public Akatsuki(String pakaian) {
+	 
+	// Tampung nilai sementara menggunakan this yang merujuk pada property pakaian
+     this.pakaian = pakaian;
+ }
 
-    public Akatsuki(String pakaian) {
-        this.pakaian = pakaian;
-    }
+ // Getter untuk mendapatkan informasi pakaian
+ public String getPakaian() {
+     return pakaian;
+ }
 
-    public String getPakaian() {
-        return pakaian;
-    }
-
-    public void tampilkanDataAkatsuki() {
-        // Method kosong atau method dengan blok kosong
-    }
+ // Metode untuk menampilkan informasi data Akatsuki
+ public void tampilkanDataAkatsuki() {
+     // Method kosong atau method dengan blok kosong
+ }
 }
 
+//Menmbuat kelas Anggota yang merupakan turunan dari Akatsuki
 class Anggota extends Akatsuki {
-    private String nama;
-    private String desa;
+	
+ // Membuat property nama, desa
+ private String nama;
+ private String desa;
 
-    public Anggota(String nama, String pakaian, String desa) {
-        super(pakaian);
-        this.nama = nama;
-        this.desa = desa;
-    }
+ // Konstruktor untuk menginisialisasi informasi nama, pakaian, dan desa
+ public Anggota(String nama, String pakaian, String desa) {
+     super(pakaian); // Keyword super mengakses pada konstruktor kelas Akatsuki pada parameter pakaian
+     
+     // Tampung nilai sementara menggunakan this yang merujuk pada property nama, peran
+     this.nama = nama;
+     this.desa = desa;
+ }
 
-    public String getNama() {
-        return nama;
-    }
-    
-    public String getDesa() {
-    	return desa;
-    }
+ // Getter untuk mendapatkan informasi nama
+ public String getNama() {
+     return nama;
+ }
 
-    @Override
-    public void tampilkanDataAkatsuki() {
-        System.out.println("Saya adalah anggota akatsuki dengan nama " +  getNama() + " menggunakan " + getPakaian() + " tinggal di desa " + getDesa() + ".");
-    }
+ // Getter untuk mendapatkan informasi desa
+ public String getDesa() {
+     return desa;
+ }
+
+ // Override metode tampilkanDataAkatsuki untuk memberikan implementasi khusus anggota Akatsuki
+ @Override
+ public void tampilkanDataAkatsuki() {
+     System.out.println("Saya adalah anggota Akatsuki dengan nama " + getNama() +
+             " menggunakan " + getPakaian() + " tinggal di desa " + getDesa() + ".");
+ }
 }
 
+//Kelas Main sebagai kelas utama
 public class Main {
 
-	public static void main(String[] args) {
-		
-		 	List<Anggota> DataAnggotaAkatsuki = new ArrayList<>();
+	// Method main sebagai method utama untuk menjalankan program
+ public static void main(String[] args) {
+     
+     // Membuat daftar anggota Akatsuki menggunakan ArrayList
+     List<Anggota> dataAnggotaAkatsuki = new ArrayList<>();
 
-		 	DataAnggotaAkatsuki.add(new Anggota("Itachi Uchiha", "pakaian hitam beserta corak awan merah", "Konohagakure"));
-		 	DataAnggotaAkatsuki.add(new Anggota("Kisame Hoshigake", "pakaian hitam beserta corak awan merah", "Kirigakure"));
-		 	DataAnggotaAkatsuki.add(new Anggota("Deidara", "pakaian hitam beserta corak awan merah", "Iwagakure"));
-		 	DataAnggotaAkatsuki.add(new Anggota("Sasori", "pakaian hitam beserta corak awan merah", "Sunagakure"));
-		 	DataAnggotaAkatsuki.add(new Anggota("Kakuzu", "pakaian hitam beserta corak awan merah", "Takigakure"));
-		 	DataAnggotaAkatsuki.add(new Anggota("Hidan", "pakaian hitam beserta corak awan merah", "Yugakure"));
-		 	DataAnggotaAkatsuki.add(new Anggota("Orochimaru", "pakaian hitam beserta corak awan merah", "Konohagakure"));
-	        
-	        for (Anggota anggota : DataAnggotaAkatsuki) {
-	            anggota.tampilkanDataAkatsuki();
-	            System.out.println("\n");
-	        }
+     // Menambahkan anggota Akatsuki ke dalam daftar
+     dataAnggotaAkatsuki.add(new Anggota("Itachi Uchiha", "pakaian hitam beserta corak awan merah", "Konohagakure"));
+     dataAnggotaAkatsuki.add(new Anggota("Kisame Hoshigake", "pakaian hitam beserta corak awan merah", "Kirigakure"));
+     dataAnggotaAkatsuki.add(new Anggota("Deidara", "pakaian hitam beserta corak awan merah", "Iwagakure"));
+     dataAnggotaAkatsuki.add(new Anggota("Sasori", "pakaian hitam beserta corak awan merah", "Sunagakure"));
+     dataAnggotaAkatsuki.add(new Anggota("Kakuzu", "pakaian hitam beserta corak awan merah", "Takigakure"));
+     dataAnggotaAkatsuki.add(new Anggota("Hidan", "pakaian hitam beserta corak awan merah", "Yugakure"));
+     dataAnggotaAkatsuki.add(new Anggota("Orochimaru", "pakaian hitam beserta corak awan merah", "Konohagakure"));
 
-	}
-
+     // Menampilkan informasi setiap anggota Akatsuki dalam daftar
+     for (Anggota anggota : dataAnggotaAkatsuki) {
+         anggota.tampilkanDataAkatsuki();
+         System.out.println("\n");
+     }
+ }
 }
